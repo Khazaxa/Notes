f70cd105d84b8bef381b3a2bb5db312222885a1d
Jakub <96346556+Khazaxa@users.noreply.github.com>
2022-12-08 21:57:18 +0100

    Do folders

---
+++ Patterns/Pattern X/PatternX/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PatternX
+{
+        class Program
+        {
+            public static void PrintX(int n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i == j || i + j == n - 1)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            static void Main(string[] args)
+            {
+                PrintX(5);
+            }
+        }    
+}
