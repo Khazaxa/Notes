cfea6771b756c7d67bd3bc9c5c67c6ac113a13e0
Jakub <96346556+Khazaxa@users.noreply.github.com>
2022-12-08 22:11:03 +0100

    Attach LED Numbers

---
+++ Patterns/Pattern Triangle/PatternTriangle/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PatternTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
