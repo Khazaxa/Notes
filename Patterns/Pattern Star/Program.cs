0d98f3ba4d7323045a569146a79fdddcab29c1be
Jakub <96346556+Khazaxa@users.noreply.github.com>
2022-12-08 22:37:00 +0100

    Create Star Pattern

---
+++ Patterns/Pattern Star/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Hourglass
+{
+    internal class Program
+    {
+        public static void PrintHourglass(int n)
+        {
+            if (n % 2 == 0)
+            {
+                n--;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j || i + j == n - 1 || i == n / 2 || j == n / 2)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintHourglass(25);
+        }
+    }
+}
---
+++ Patterns/Pattern Z/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PatternZ
+{
+    internal class Program
+    {
+        public static void PrintZ(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 || i == n - 1 || i + j == n - 1)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintZ(12);
+        }
+    }
+}
